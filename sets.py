68c43dcf396399372f24779b531701fea75d83a4
Tony Ni <zhni18@uwcchina.org>
2024-10-18 22:00:06 -0500

    Recon Backups

--- sets.py
+++ sets.py
@@ -4,16 +4,17 @@ def main(blast_file, orf_file):
     try:
         # Reading the BLAST file and storing ORF names
         with open(blast_file, 'r') as infile:
-            blast_orf_names = [line.split('\t')[1] for line in infile]
+            blast_orf_names = [line.split('\t')[0] for line in infile]
         
         # Output file named after the ORF file with '_BLASThits.txt' suffix
-        output_file = f"{orf_file}_BLASThits.txt"
+        output_file = f"{orf_file}_BLASThitstttt.txt"
         
         # Process the ORF coverage file and output matching lines
         with open(orf_file, 'r') as infile, open(output_file, 'w') as outfile:
+            next(infile)  # Skip header if there is one
             for line in infile:
                 cols = line.split('\t')
-                orf_title = cols[4]
+                orf_title = cols[4]  # Change index here if the index is cols[0]
                 if orf_title in blast_orf_names:
                     outfile.write(line)
         
